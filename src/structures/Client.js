aef23ab9744dc0ebe693f6f3a4c5e6fdab3e95a5
iCrawl <icrawltogo@gmail.com>
2018-02-10 11:02:35 +0100

    refactor: add events to the registry

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,8 +1,6 @@
 const { EventEmitter } = require('events');
 const rest = require('@spectacles/rest');
 const { Amqp } = require('@spectacles/brokers');
-const { readdirSync } = require('fs');
-const { extname, join } = require('path');
 const Dispatcher = require('../structures/Dispatcher');
 const Registry = require('../structures/Registry');
 
@@ -12,7 +10,6 @@ class Strelitzia extends EventEmitter {
 		this.rest = rest(options.token);
 		this.id = options.id;
 		this.prefix = options.prefix || '=';
-		this.eventPath = options.eventPath;
 		this.consumer = new Amqp('consumer');
 		this.dispatcher = new Dispatcher(this);
 		this.registry = new Registry(this);
@@ -22,14 +19,6 @@ class Strelitzia extends EventEmitter {
 		try {
 			await this.consumer.connect(url);
 			await this.consumer.subscribe(events);
-			// TODO: Needs refactoring into a registry or something
-			const files = readdirSync(this.eventPath);
-			for (let event of files) {
-				if (extname(event) !== '.js') continue;
-				event = require(join(this.eventPath, event));
-				if (typeof event === 'function') event = new event(this);
-				if (event.enabled) this.consumer.on(event.name, event._run.bind(event));
-			}
 		} catch (error) {
 			console.error(error);
 		}
--- src/structures/Registry.js
+++ src/structures/Registry.js
@@ -1,8 +1,39 @@
+const { readdirSync } = require('fs');
+const { extname, join } = require('path');
+
 class Registry {
 	constructor(client) {
 		Object.defineProperty(this, 'client', { value: client });
 
+		this.events = new Map();
 		this.commands = new Map();
+	}
+
+	registerEvent(event) {
+		return this.registerEvents([event]);
+	}
+
+	registerEvents(events) {
+		if (!Array.isArray(events)) return;
+		for (let event of events) {
+			if (typeof event === 'function') event = new event(this.client);
+
+			this.events.set(event.name, event);
+
+			if (event.enabled) this.client.consumer.on(event.name, event._run.bind(event));
+		}
+	}
+
+	registerEventsIn(path) {
+		const files = readdirSync(path);
+		const events = [];
+		for (let event of files) {
+			if (extname(event) !== '.js') continue;
+			event = require(join(path, event));
+			events.push(event);
+		}
+
+		return this.registerEvents(events);
 	}
 
 	registerCommand(command) {
@@ -18,7 +49,17 @@ class Registry {
 		}
 	}
 
-	registerCommandsIn() {}
+	registerCommandsIn(path) {
+		const files = readdirSync(path);
+		const commands = [];
+		for (let command of files) {
+			if (extname(command) !== '.js') continue;
+			command = require(join(path, command));
+			commands.push(command);
+		}
+
+		return this.registerCommands(commands);
+	}
 }
 
 module.exports = Registry;
