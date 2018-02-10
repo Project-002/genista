4b1bf07cf0e78d3cd4714be8fcd870882a170a2d
iCrawl <icrawltogo@gmail.com>
2018-02-10 10:50:29 +0100

    feat: barebones event

---
+++ src/structures/Event.js
@@ -0,0 +1,22 @@
+class Event {
+	constructor(client, options = {}) {
+		Object.defineProperty(this, 'client', { value: client });
+		this.name = options.name;
+		this.type = 'event';
+		this.enabled = Boolean(options.enabled);
+	}
+
+	async _run(...args) {
+		if (this.enabled) {
+			try {
+				await this.run(...args);
+			} catch (error) {
+				this.client.emit('error', this, args, error);
+			}
+		}
+	}
+
+	run() {}
+}
+
+module.exports = Event;
