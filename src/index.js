f536f06eeb1b086c3d2a630468c52b50e0945a5b
iCrawl <icrawltogo@gmail.com>
2018-02-10 10:49:41 +0100

    feat: expose event and command

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = {
 	Strelitzia: require('./structures/Client'),
-	Client: require('./structures/Client')
+	Client: require('./structures/Client'),
+
+	Event: require('./structures/Event'),
+	Command: require('./structures/Command')
 };
