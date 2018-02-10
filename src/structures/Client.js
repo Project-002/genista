26942f6e7f75c235ff1ba76daee50fad16c9e5d1
iCrawl <icrawltogo@gmail.com>
2018-02-10 10:50:51 +0100

    feat: add registry and dispatcher to the client and add default values

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -2,19 +2,20 @@ const { EventEmitter } = require('events');
 const rest = require('@spectacles/rest');
 const { Amqp } = require('@spectacles/brokers');
 const { readdirSync } = require('fs');
-const { join } = require('path');
+const { extname, join } = require('path');
+const Dispatcher = require('../structures/Dispatcher');
+const Registry = require('../structures/Registry');
 
 class Strelitzia extends EventEmitter {
 	constructor(options = {}) {
 		super();
 		this.rest = rest(options.token);
-		this.prefix = null;
+		this.id = options.id;
+		this.prefix = options.prefix || '=';
 		this.eventPath = options.eventPath;
 		this.consumer = new Amqp('consumer');
-	}
-
-	get prefix() {
-		return this._prefix;
+		this.dispatcher = new Dispatcher(this);
+		this.registry = new Registry(this);
 	}
 
 	async login(url = 'localhost', events) {
@@ -24,6 +25,7 @@ class Strelitzia extends EventEmitter {
 			// TODO: Needs refactoring into a registry or something
 			const files = readdirSync(this.eventPath);
 			for (let event of files) {
+				if (extname(event) !== '.js') continue;
 				event = require(join(this.eventPath, event));
 				if (typeof event === 'function') event = new event(this);
 				if (event.enabled) this.consumer.on(event.name, event._run.bind(event));
